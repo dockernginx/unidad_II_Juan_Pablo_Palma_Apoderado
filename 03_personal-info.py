fd8ff438a2d390f6095263468959c27c84acacb2
pablo0121 <pablopalma012@gmail.com>
2022-10-18 09:22:36 -0500

    Ejercicios Unidad II

--- 03_personal-info.py
+++ 03_personal-info.py
@@ -3,9 +3,9 @@ Create a script that asks for four pieces of information such as: first name, la
 Create a variable for a space: space = " "
 Add a print statement that that combines all the information in one sentence.
 Run the script and troubleshoot any errors'''
-name = input('What is your name? ')
-lsname = input('what is your last name? ')
-location = input('Where are you? ')
-age = input('How are old? ')
+name = input('Cual es tu nombre? ')
+lsname = input('Cual es tu apellido? ')
+location = input('Donde te encuentras? ')
+age = input('Que edad tienes? ')
 space = ''
 print('hello ',name , space, lsname,'location is: ', space, location,'age: ', age)
