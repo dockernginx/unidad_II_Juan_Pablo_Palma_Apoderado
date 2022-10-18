c76a365920ab409e8d8c0ca2d2a27f6b17c78b0f
pablo0121 <pablopalma012@gmail.com>
2022-10-18 09:17:29 -0500

    Ejercicios Unidad II

---
+++ 03_personal-info.py
@@ -0,0 +1,11 @@
+'''Open a blank script file and save it in your GitHub project directory as 03_personal-info.py.
+Create a script that asks for four pieces of information such as: first name, last name, location, and age.
+Create a variable for a space: space = " "
+Add a print statement that that combines all the information in one sentence.
+Run the script and troubleshoot any errors'''
+name = input('What is your name? ')
+lsname = input('what is your last name? ')
+location = input('Where are you? ')
+age = input('How are old? ')
+space = ''
+print('hello ',name , space, lsname,'location is: ', space, location,'age: ', space, age)
